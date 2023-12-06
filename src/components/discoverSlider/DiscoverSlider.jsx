67ffa2b740ed2229010b090d20daa92a8cb1cdab
Mohamed Amged <mohamedamged55555@gmail.com>
2023-12-06 21:33:52 +0200

    adding more than slider in discover page

---
+++ src/components/discoverSlider/DiscoverSlider.jsx
@@ -0,0 +1,150 @@
+import React from 'react'
+import './discoverSlider.css'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+export default function DiscoverSlider({ title, subtitle, slides }) {
+    return (
+        // <div className='discoverSlider__handler'>
+        //     <div className="container">
+        //         <div className="row">
+        //             <div className="col-lg-8 col-md-8 col-sm-12">
+        //                 <div className="discoverSlider__head">
+        //                     <h3>
+        //                         Most recommended food categories
+        //                     </h3>
+        //                     <p>
+        //                         winning flavors for every appetite
+        //                     </p>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //         <div className="row">
+        //             <div className="col-lg-12">
+        //                 <div className="discoverSlider__cards">
+        //                     <Swiper
+        //                         slidesPerView={3.5}
+        //                         spaceBetween={30}
+        //                         className="mySwiper"
+        //                     >
+        //                         <SwiperSlide>
+        //                             <div className="discoverSlider__card__item">
+        //                                 <div className="overlay"></div>
+        //                                 <div className="discoverSlider__img">
+        //                                     <img src={foodImg} alt="food__img" />
+        //                                 </div>
+        //                                 <div className="discoverSlider__text">
+        //                                     <h3>
+        //                                         fine Dining
+        //                                     </h3>
+        //                                 </div>
+        //                             </div>
+        //                         </SwiperSlide>
+        //                         <SwiperSlide>
+        //                             <div className="discoverSlider__card__item">
+        //                                 <div className="overlay"></div>
+        //                                 <div className="discoverSlider__img">
+        //                                     <img src={foodImg} alt="food__img" />
+        //                                 </div>
+        //                                 <div className="discoverSlider__text">
+        //                                     <h3>
+        //                                         fine Dining
+        //                                     </h3>
+        //                                 </div>
+        //                             </div>
+        //                         </SwiperSlide>
+        //                         <SwiperSlide>
+        //                             <div className="discoverSlider__card__item">
+        //                                 <div className="overlay"></div>
+        //                                 <div className="discoverSlider__img">
+        //                                     <img src={foodImg} alt="food__img" />
+        //                                 </div>
+        //                                 <div className="discoverSlider__text">
+        //                                     <h3>
+        //                                         fine Dining
+        //                                     </h3>
+        //                                 </div>
+        //                             </div>
+        //                         </SwiperSlide>
+        //                         <SwiperSlide>
+        //                             <div className="discoverSlider__card__item">
+        //                                 <div className="overlay"></div>
+        //                                 <div className="discoverSlider__img">
+        //                                     <img src={foodImg} alt="food__img" />
+        //                                 </div>
+        //                                 <div className="discoverSlider__text">
+        //                                     <h3>
+        //                                         fine Dining
+        //                                     </h3>
+        //                                 </div>
+        //                             </div>
+        //                         </SwiperSlide>
+        //                         <SwiperSlide>
+        //                             <div className="discoverSlider__card__item">
+        //                                 <div className="overlay"></div>
+        //                                 <div className="discoverSlider__img">
+        //                                     <img src={foodImg} alt="food__img" />
+        //                                 </div>
+        //                                 <div className="discoverSlider__text">
+        //                                     <h3>
+        //                                         fine Dining
+        //                                     </h3>
+        //                                 </div>
+        //                             </div>
+        //                         </SwiperSlide>
+        //                         <SwiperSlide>
+        //                             <div className="discoverSlider__card__item">
+        //                                 <div className="overlay"></div>
+        //                                 <div className="discoverSlider__img">
+        //                                     <img src={foodImg} alt="food__img" />
+        //                                 </div>
+        //                                 <div className="discoverSlider__text">
+        //                                     <h3>
+        //                                         fine Dining
+        //                                     </h3>
+        //                                 </div>
+        //                             </div>
+        //                         </SwiperSlide>
+        //                     </Swiper>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+        <div className="discoverSlider__handler">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-8 col-md-8 col-sm-12">
+                        <div className="discoverSlider__head">
+                            <h3>{title}</h3>
+                            <p>{subtitle}</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="discoverSlider__cards">
+                            <Swiper 
+                            slidesPerView={3.5} 
+                            spaceBetween={30} 
+                            className="mySwiper">
+                                {slides.map((slide) => (
+                                    <SwiperSlide key={slide.id}>
+                                        <div className="discoverSlider__card__item">
+                                            <div className="overlay"></div>
+                                            <div className="discoverSlider__img">
+                                                <img src={slide.image} alt={`food_${slide.id}`} />
+                                            </div>
+                                            <div className="discoverSlider__text">
+                                                <h3>{slide.category}</h3>
+                                            </div>
+                                        </div>
+                                    </SwiperSlide>
+                                ))}
+                            </Swiper>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/discoverHome/DiscoverHome.jsx
+++ src/pages/discoverHome/DiscoverHome.jsx
@@ -4,11 +4,55 @@ import DynamicHero from '../../components/dynamicHeroPart/DynamicHero'
 import DiscoverAdsCateg from '../../components/discoverAdsCategories/DiscoverAdsCateg'
 import DiscoverAdsLocation from '../../components/discoverAdsLocation/DiscoverAdsLocation'
 import DiscoverAdsFilter from '../../components/discoverAdsFilter/DiscoverAdsFilter'
+import DiscoverSlider from '../../components/discoverSlider/DiscoverSlider'
+import foodImg from '../../assets/discoverHomeImg/food1.jpg'
+import burger from '../../assets/discoverHomeImg/food3.jpg'
+import cafe1 from '../../assets/discoverHomeImg/cafe1.jpg'
+import cafe2 from '../../assets/discoverHomeImg/cafe2.jpg'
+import cafe3 from '../../assets/discoverHomeImg/cafe3.jpg'
+import cafe4 from '../../assets/discoverHomeImg/cafe4.jpg'
+import gym from '../../assets/discoverHomeImg/health2.jpg'
+import gym1 from '../../assets/discoverHomeImg/health1.jpg'
+import gym2 from '../../assets/discoverHomeImg/health.jpg'
+const slidesOne = [
+  { "id": 1, "category": 'Fine Dining', "image": foodImg },
+  { "id": 2, "category": 'Italian Cuisine', "image": foodImg },
+  { "id": 3, "category": 'Seafood Delights', "image": foodImg },
+  { "id": 3, "category": 'Seafood Delights', "image": foodImg },
+  { "id": 3, "category": 'Seafood Delights', "image": foodImg },
+  { "id": 3, "category": 'Seafood Delights', "image": foodImg },
+  { "id": 3, "category": 'Seafood Delights', "image": foodImg }
+];
+const slidesTwo = [
+  { "id": 1, "category": 'Fine Dining', "image": burger },
+  { "id": 2, "category": 'Italian Cuisine', "image": burger },
+  { "id": 3, "category": 'Seafood Delights', "image": burger },
+  { "id": 3, "category": 'Seafood Delights', "image": burger },
+  { "id": 3, "category": 'Seafood Delights', "image": burger },
+  { "id": 3, "category": 'Seafood Delights', "image": burger },
+  { "id": 3, "category": 'Seafood Delights', "image": burger }
+];
+const slidesThree = [
+  { "id": 1, "category": 'cafe one', "image": cafe1 },
+  { "id": 2, "category": 'cafe two', "image": cafe2 },
+  { "id": 3, "category": 'cafe three', "image": cafe3 },
+  { "id": 3, "category": 'cafe four', "image": cafe4 },
+];
+const slidesFour = [
+  { "id": 1, "category": 'gym one', "image": gym },
+  { "id": 2, "category": 'gym two', "image": gym1 },
+  { "id": 3, "category": 'gym three', "image": gym2 },
+  { "id": 1, "category": 'gym one', "image": gym },
+];
 export default function DiscoverHome() {
   return (
     <div>
       <DynamicHero />
+      <DiscoverSlider title="Most recommended food categories" subtitle="Winning flavors for every appetite" slides={slidesOne}/>
       <DiscoverAdsCateg />
+      <DiscoverSlider title=" Most recommend burgers in country" subtitle="Winning flavors for every appetite" slides={slidesTwo}/>
+      <DiscoverSlider title=" Most recommend cafe in country" subtitle="Winning flavors for every appetite" slides={slidesThree}/>
+      <DiscoverSlider title="Most recommended gym fit" subtitle="Winning flavors for every appetite" slides={slidesFour}/>
       <DiscoverAdsLocation />
       <DiscoverAdsFilter />
     </div>
